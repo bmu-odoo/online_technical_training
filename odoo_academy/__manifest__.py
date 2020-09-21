26467fa64aa5875341b7eb67eaf395c365cce19c
Ben Mustion <bmu@odoo.com>
2020-09-21 17:54:48 +0000

    [ADD] ORM

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -1,31 +1,33 @@
 # -*- coding: utf-8 -*-
-
 {
     'name': 'Odoo Academy',
-    
-    'summary': """Academy app to manage Training""",
-    
+
+    'summary': """Academy app to manage training""",
+
     'description': """
-        Academy Module to manage Training:
-        - Courses
-        - Sessions
-        - Attendees
+        Academy Module to manage training:
+        - courses
+        - sessions
+        - attendees
     """,
-    
+
     'author': 'Odoo',
-    
     'website': 'https://www.odoo.com',
-    
+
     'category': 'Training',
     'version': '0.1',
-    
+
     'depends': ['base'],
-    
+
     'data': [
-        
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
+        'views/academy_menuitem.xml',
+        'views/academy_views.xml',
     ],
-    
+
     'demo': [
-        
+        'demo/academy_demo.xml',
     ],
+
 }
